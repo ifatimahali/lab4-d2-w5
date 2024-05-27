8ec8914905352eb0b6407519c85d55fcbdfa2ed4
ifatimahali <star18salfa@gmail.com>
2024-05-27 20:56:24 +0300

    f

---
+++ main.js
@@ -0,0 +1,17 @@
+let input = document.querySelectorAll('input');
+let squere = document.getElementById('Squere');
+      
+function b(params) {
+    squere.style.backgroundColor =params    
+}
+function widthS(params) {
+    squere.style.width =params + "vw" ;
+}
+function heightS(params) {
+    squere.style.height = params + "vh" ;
+}
+function borderR(params) {
+    squere.style.borderRadius = params + "%" ;
+}
+
+
